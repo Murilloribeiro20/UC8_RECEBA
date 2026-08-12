d54f63e354a70e92b6bc9d5a1f6c2ee7937961b0
Murillo <m7olivzxr@gmail.com>
2026-08-12 10:59:13 -0300

    Add files via upload

---
+++ missao 112/js/script.js
@@ -0,0 +1,20 @@
+const btnGravador = document.getElementById('btn-gravador');
+const statusGravador = document.getElementById('status-gravacao');
+
+btnGravador.addEventListener('pointerdown', function() {
+    btnGravador.innerHTML = '🔴 Gravando... Não solte!'
+    btnGravador.style.backgroundColor = '#e74c3c';
+    statusGravador.innerHTML = 'Status: Capturando áudio...';
+});
+
+btnGravador.addEventListener('pointerup', function() {
+    btnGravador.innerHTML = '🎤 Pressione e segure para gravar';
+    btnGravador.style.backgroundColor = '#3498db';
+    statusGravador.innerHTML = 'Status: Gravação concluída e enviada!';
+});
+
+btnGravador.addEventListener('pointercancel', function() {
+    btnGravador.innerHTML = '🛑 Gravação cancelada!';
+    btnGravador.style.backgroundColor = '#9ca0a3';
+    statusGravador.innerHTML = 'Status: Gravação cancelada! Aperte e segure novamente para gravar.';
+});
